6a807f9f3624ea810465373b44db6ec21bb6f1f0
Collin Duncan <cgduncan7@gmail.com>
2019-12-09 22:24:56 +0100

    Removed unnecessary stuff

--- liveframe-ui/src/router/index.ts
+++ liveframe-ui/src/router/index.ts
@@ -14,14 +14,6 @@ const routes = [
     path: '/window',
     name: 'window',
     component: () => import(/* webpackChunkName: "window" */ '../views/Window.vue')
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
--- liveframe-ui/src/views/Home.vue
+++ liveframe-ui/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+    <img alt="Vue logo" src="../assets/logo_small.png">
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+<style lang="scss">
+#home {
+  height: 100%;
+  width: 100%;
+  background: #777777;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 }
-</script>
+</style>
