c091697b7cd2b5de4eb8e556548f7d3cbbc07dd4
Collin Duncan <cgduncan7@gmail.com>
2020-02-09 19:40:36 +0100

    added cors support to server

--- liveframe-server/index.js
+++ liveframe-server/index.js
@@ -1,6 +1,9 @@
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
+const cors = require('cors')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('<h1>howdy doody</h1>')
