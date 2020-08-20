cbb6ffdf5881b239a1ac16c02786d9893c9ff44d
Collin Duncan <cgduncan7@gmail.com>
2020-08-20 17:27:16 +0200

    Added authenticator for auth to images endpoint

---
+++ liveframe-server/authenticator.js
@@ -0,0 +1,28 @@
+const authenticator = (req, res, next) => {
+  const auth = req.header('authorization')
+
+  if (auth === undefined) {
+    res.sendStatus(401)
+    return
+  }
+
+  const [authType, encodedToken] = auth.split(' ')
+
+  if (authType.toLowerCase() !== 'basic') {
+    res.sendStatus(401)
+    return
+  }
+
+  const decodedToken = Buffer.alloc(encodedToken.length * 3 / 4, encodedToken, 'base64').toString()
+
+  const [user, pass] = decodedToken.split(':')
+
+  if (user === process.env.USERNAME&& pass === process.env.PASSWORD) {
+    next()
+  } else {
+    res.sendStatus(401)
+    return
+  }
+}
+
+module.exports = authenticator
